e3e04f3c1a225798594f031017e7f50b1c5d5d13
FabianBautista <fabian.bautista08@gmail.com>
2017-04-06 23:37:14 -0300

    Add files via upload

---
+++ T.P_1_Calculadora-master/funciones.h
@@ -0,0 +1,10 @@
+#ifndef LIBRERIA_H_INCLUDED
+#define LIBRERIA_H_INCLUDED
+
+float suma(float x, float y);
+float resta(float x, float y);
+float multiplicacion(float x,float y);
+float division(float x,float y);
+int factorial2(int x);
+
+#endif // LIBRERIA_H_INCLUDED
